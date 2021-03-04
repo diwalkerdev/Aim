0f32f2f30debf18b501003b16e57e9e8557d4f2d
David Walker <dwalker0044@gmail.com>
2021-03-04 12:29:37 +0000

    Removed old directory.

--- test/project/app/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "add.h"
-
-int main() {
-    int result = add(5,3);
-    printf("The answer is: %d", result);
-    return 0;
-}
--- test/project/libproject/greet.h
+++
@@ -1,6 +0,0 @@
-#ifndef GREET_H
-#define GREET_H
-
-void say_hello();
-
-#endif # GREET_H
