5f5e4d16df2b2640d091fa5a4c905d55214e0d9a
David Walker <dwalker0044@gmail.com>
2021-05-13 21:39:49 +0100

    Fixing src includes.

--- aim-dev.bat
+++ aim-dev.bat
@@ -1,3 +1,3 @@
 @echo off
-set PYTHONPATH=%cd%\src
-for /f %%i in ('poetry env info -p') do %%i\Scripts\python %cd%\src\aim_build\main.py
+set PYTHONPATH=%cd%
+for /f %%i in ('poetry env info -p') do %%i\Scripts\python %cd%\aim_build\main.py
