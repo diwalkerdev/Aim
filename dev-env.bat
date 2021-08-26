a5ad84713c877d2323fb1b6cb9f89278ebe3ad2d
David Walker <dwalker0044@gmail.com>
2021-08-26 10:21:32 +0100

    Improved dev-env.bat. Now execute aim using C:\Users\dwalk\AppData\Local\pypoetry\Cache\virtualenvs\aim-build-8D_wjejt-py3.9\Scripts\python C:\Users\dwalk\Projects\Aim\aim_build\main.py <args>.

--- dev-env.bat
+++ dev-env.bat
@@ -1,6 +1,13 @@
 @echo off
-REM set PYTHONPATH=%cd%
-REM for /f %%i in ('poetry env info -p') do %%i\Scripts\python %cd%\aim_build\main.py
-
 set PYTHONPATH=%cd%
-set AIMCMD=poetry run python %cd%\aim_build\main.py
+echo PYTHONPATH: %PYTHONPATH%
+
+REM Getting the path to python works better than poetry run, because poetry run
+REM requires that you are in the aim directory for it to work, and there's no
+REM option for switching into another directory like -C<some dir>.
+REM Because this is how you expand commands in batch scripts. Facepalm.
+for /f %%i in ('poetry env info -p') do set AIM_POETRY=%%i
+echo AIM_POETRY: %AIM_POETRY%
+
+set "aimcmd=%AIM_POETRY%\Scripts\python %cd%\aim_build\main.py"
+echo aimcmd:     %aimcmd%
